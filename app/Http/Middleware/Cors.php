d5a49e11fcee9b32ed8bb55b881df1c501d2ba44
petertecnetdev <petertecnet@gmail.com>
2025-02-26 14:04:13 -0300

    New one cors

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class Cors
+{
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->header('Access-Control-Allow-Headers', '*');
+
+        return $response;
+    }
+}
