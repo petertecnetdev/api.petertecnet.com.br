6382682dd174b9e2a78809f7824c42dc6ea12172
petertecnetdev <petertecnet@gmail.com>
2025-02-27 10:06:53 -0300

    all views

--- storage/framework/views/0956cd4e13cc74de4d383e2bb22b3ac8f301cf93.php
+++ storage/framework/views/0956cd4e13cc74de4d383e2bb22b3ac8f301cf93.php
@@ -62,10 +62,10 @@
 
 <body>
     <div class="content">
-        <img src="images/peterlogo.png" alt="Logo Peter Tecnet" class="logo">
+        <img src="/images/peterlogo.png" alt="Logo Peter Tecnet" class="logo">
         <p class="welcome-message">Peter Tecnet</p>
-   Env     <p class="text-danger text-uppercase h4">{{ config('app.env') }}</p>
+   Env     <p class="text-danger text-uppercase h4"><?php echo e(config('app.env')); ?></p>
     </div>
 </body>
 
-</html>
+</html><?php /**PATH C:\Development\xamp\htdocs\api.petertecnet.com.br\resources\views/welcome.blade.php ENDPATH**/ ?>
