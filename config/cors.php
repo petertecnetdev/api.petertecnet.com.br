72fbb17e18d5a609939967d0f5b36724f62fb131
petertecnetdev <petertecnet@gmail.com>
2025-03-16 17:24:05 -0300

    service by entity

--- config/cors.php
+++ config/cors.php
@@ -5,16 +5,19 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
+
+    // Se você não precisa enviar cookies ou autenticação via credenciais, defina como false.
+    // Usar '*' com suporte a credenciais (true) não é permitido.
+    'supports_credentials' => false,
+
     'allowed_origins' => ['*'],
 
-    'allowed_origins_patterns' => ['*'],
+    // Se 'allowed_origins' for '*', não é necessário definir padrões de origem.
+    'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => ['*'],
+    'exposed_headers' => [],
 
     'max_age' => 0,
-
-    'supports_credentials' => true,
-
 ];
