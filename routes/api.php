4e07ed638b83ed8dbdd2e429fab69bacd39a8506
petertecnetdev <petertecnet@gmail.com>
2025-03-16 21:41:18 -0300

    solving conflit develop and staging on local

--- routes/api.php
+++ routes/api.php
@@ -17,10 +17,9 @@ use App\Http\Controllers\{
     ServiceRecordController
 };
 
-
 Route::group([
     'middleware' => 'api',
-    'prefix' => 'auth'
+    'prefix'     => 'auth'
 ], function ($router) {
     Route::post('/login', [AuthController::class, 'login']);
     Route::post('/register', [AuthController::class, 'register'])->name('register');
@@ -32,13 +31,14 @@ Route::group([
     Route::get('/check-auth', [AuthController::class, 'checkauth'])->middleware('auth:api')->name('checkAuth');
     Route::post('/email-verify', [AuthController::class, 'emailVerify'])->middleware('auth:api')->name('emailVerify');
     Route::post('/change-password', [AuthController::class, 'changePassword'])->middleware('auth:api')->name('changePassword');
-    Route::post('/resend-code-email-verification', [AuthController::class, 'resendCodeEmailVerification'])->middleware('auth:api')->name('resendVerificationCode');
+    Route::post('/resend-code-email-verification', [AuthController::class, 'resendCodeEmailVerification'])
+        ->middleware('auth:api')
+        ->name('resendVerificationCode');
 });
-
 
 Route::group([
     'middleware' => 'api',
-    'prefix' => 'user'
+    'prefix'     => 'user'
 ], function ($router) {
     Route::get('/', [UserController::class, 'list'])->name('user.list');
     Route::get('/show/{id}', [UserController::class, 'show'])->name('user.show');
@@ -48,11 +48,9 @@ Route::group([
     Route::delete('/{id}', [UserController::class, 'destroy'])->name('user.destroy');
 });
 
-
-
 Route::group([
     'middleware' => 'api',
-    'prefix' => 'profile'
+    'prefix'     => 'profile'
 ], function ($router) {
     Route::get('/', [ProfileController::class, 'list'])->name('profile.list');
     Route::get('/{id}', [ProfileController::class, 'show'])->name('profile.show');
@@ -63,7 +61,7 @@ Route::group([
 
 Route::group([
     'middleware' => 'api',
-    'prefix' => 'production'
+    'prefix'     => 'production'
 ], function ($router) {
     Route::post('/', [ProductionController::class, 'store'])->name('production.store');
     Route::get('/', [ProductionController::class, 'list'])->name('production.list');
@@ -71,13 +69,13 @@ Route::group([
     Route::post('/{id}', [ProductionController::class, 'update'])->name('production.update');
     Route::delete('/{id}', [ProductionController::class, 'delete'])->name('production.delete');
     Route::get('/{slug}', [ProductionController::class, 'view'])->name('production.view');
-    Route::get('/cnpj/get-company-info', [ProductionController::class, 'getCompanyInfo'])->name('production.getCompanyInfo');
-
+    Route::get('/cnpj/get-company-info', [ProductionController::class, 'getCompanyInfo'])
+        ->name('production.getCompanyInfo');
 });
 
 Route::group([
     'middleware' => 'api',
-    'prefix' => 'event'
+    'prefix'     => 'event'
 ], function ($router) {
     Route::post('/', [EventController::class, 'store'])->name('event.store');
     Route::get('/', [EventController::class, 'list'])->name('event.list');
@@ -87,9 +85,10 @@ Route::group([
     Route::get('/{slug}', [EventController::class, 'view'])->name('event.view');
     Route::get('/myevents/list', [EventController::class, 'myEvents'])->name('event.myevents');
 });
+
 Route::group([
     'middleware' => 'api',
-    'prefix' => 'ticket'
+    'prefix'     => 'ticket'
 ], function ($router) {
     Route::get('/', [TicketController::class, 'list'])->name('ticket.list');
     Route::get('/show/{id}', [TicketController::class, 'show'])->name('ticket.show');
@@ -101,25 +100,30 @@ Route::group([
     Route::get('/production/{productionId}', [TicketController::class, 'listByProduction'])->name('ticket.listByProduction');
 });
 
+Route::group([
+    'middleware' => 'api',
+    'prefix'     => 'barbershop'
+], function ($router) {
+    Route::get('/', [BarbershopController::class, 'list'])->name('barbershop.list');
+    Route::get('/show/{id}', [BarbershopController::class, 'show'])->name('barbershop.show');
+    Route::get('/view/{slug}', [BarbershopController::class, 'view'])->name('barbershop.view');
+});
 
+Route::group([
+    'middleware' => ['api', 'auth:api'],
+    'prefix'     => 'barbershop'
+], function ($router) {
+    Route::post('/', [BarbershopController::class, 'store'])->name('barbershop.store');
+    Route::get('/myBarbershops', [BarbershopController::class, 'myBarbershops'])->name('barbershop.myBarbershops');
+    Route::post('/{id}', [BarbershopController::class, 'update'])->name('barbershop.update');
+    Route::delete('/{id}', [BarbershopController::class, 'destroy'])->name('barbershop.destroy');
+    Route::get('/user', [BarbershopController::class, 'listByUser'])->name('barbershop.listByUser');
+});
 
 Route::group([
     'middleware' => 'api',
-    'prefix' => 'barbershop'
-], function ($router) {
-    Route::post('/', [BarberShopController::class, 'store'])->name('barbershop.store'); // Criar um novo serviço de barbearia
-    Route::get('/myBarbershops', [BarberShopController::class, 'myBarbershops'])->name('barbershop.myBarbershops'); // Listar todos os serviços de barbearia
-    Route::get('/', [BarberShopController::class, 'list'])->name('barbershop.list'); // Listar todos os serviços de barbearia
-    Route::get('/show/{id}', [BarberShopController::class, 'show'])->name('barbershop.show'); // Mostrar detalhes de um serviço específico
-    Route::get('/view/{slug}', [BarberShopController::class, 'view'])->name('barbershop.view'); // Mostrar detalhes de um serviço específico
-    Route::post('/{id}', [BarberShopController::class, 'update'])->name('barbershop.update'); // Atualizar um serviço de barbearia existente
-    Route::delete('/{id}', [BarberShopController::class, 'destroy'])->name('barbershop.destroy'); // Excluir um serviço de barbearia
-    Route::get('/user', [BarberShopController::class, 'listByUser'])->name('barbershop.listByUser'); // Listar barbearias do usuário autenticado
-});
-
-
-// Rotas de notícias
-Route::group(['middleware' => 'api', 'prefix' => 'news'], function () {
+    'prefix'     => 'news'
+], function () {
     Route::post('/', [NewsController::class, 'store'])->name('news.store');
     Route::get('/', [NewsController::class, 'list'])->name('news.list');
     Route::get('/show/{id}', [NewsController::class, 'show'])->name('news.show');
@@ -127,12 +131,11 @@ Route::group(['middleware' => 'api', 'prefix' => 'news'], function () {
     Route::delete('/{id}', [NewsController::class, 'destroy'])->name('news.destroy');
     Route::get('/search', [NewsController::class, 'search'])->name('news.search');
     Route::post('/{id}/comment', [NewsController::class, 'comment'])->name('news.comment');
-});// Rotas de barbeiros
-
+});
 
 Route::group([
     'middleware' => 'api',
-    'prefix' => 'barber'
+    'prefix'     => 'barber'
 ], function () {
     Route::post('/', [BarberController::class, 'store'])->name('barber.store');
     Route::delete('/', [BarberController::class, 'destroy'])->name('barber.destroy');
@@ -142,10 +145,9 @@ Route::group([
     Route::post('/{id}', [BarberController::class, 'update'])->name('barber.update');
 });
 
-// Rotas de agendamentos
 Route::group([
     'middleware' => 'api',
-    'prefix' => 'appointment'
+    'prefix'     => 'appointment'
 ], function ($router) {
     Route::post('/', [AppointmentController::class, 'store'])->name('appointment.store');
     Route::get('/listmy', [AppointmentController::class, 'listMy'])->name('appointment.listMy');
@@ -156,46 +158,41 @@ Route::group([
     Route::patch('/{id}/status', [AppointmentController::class, 'updateStatus'])->name('appointment.updateStatus');
 });
 
-
-// Rotas públicas de listagem de itens (sem autenticação)
 Route::group([
     'middleware' => 'api',
-    'prefix' => 'item'
+    'prefix'     => 'item'
 ], function ($router) {
     Route::get('/', [ItemController::class, 'listByEntity'])->name('item.listByEntity');
     Route::get('/listbyapp', [ItemController::class, 'listAll'])->name('item.listByApp');
     Route::get('/listall', [ItemController::class, 'listAll'])->name('item.listAll');
     Route::get('/listservicesbyentity', [ItemController::class, 'listServicesByEntity'])->name('item.listServicesByEntity');
-    Route::get('/{id}', [ItemController::class, 'show'])->name('item.show'); // Exibir um item específico
+    Route::get('/{id}', [ItemController::class, 'show'])->name('item.show');
 });
 
-// Rotas protegidas (exigem autenticação)
 Route::group([
     'middleware' => ['api', 'auth:api'],
-    'prefix' => 'item'
+    'prefix'     => 'item'
 ], function ($router) {
     Route::post('/', [ItemController::class, 'store'])->name('item.store');
     Route::delete('/{id}', [ItemController::class, 'destroy'])->name('item.destroy');
     Route::post('/{id}', [ItemController::class, 'update'])->name('item.update');
 });
 
-// Rotas para geração de relatórios
 Route::group([
     'middleware' => 'api',
-    'prefix' => 'report'
+    'prefix'     => 'report'
 ], function ($router) {
     Route::post('/generate', [ReportController::class, 'generatePDF'])->name('report.generate');
 });
 
-// Rotas de atendimentos (service_records)
 Route::group([
     'middleware' => 'api',
-    'prefix' => 'service-record'
+    'prefix'     => 'service-record'
 ], function ($router) {
-    Route::post('/', [ServiceRecordController::class, 'store'])->name('service_record.store'); // Registrar um novo atendimento
-    Route::get('/listmy', [ServiceRecordController::class, 'listMy'])->name('service_record.listMy'); // Listar os atendimentos do usuário autenticado
-    Route::get('/listbyclient', [ServiceRecordController::class, 'listByClient'])->name('service_record.listByClient'); // Listar atendimentos de um cliente específico
-    Route::get('/listbyentity', [ServiceRecordController::class, 'listByEntity'])->name('service_record.listByEntity'); // Listar atendimentos por entidade
-    Route::get('/listbyprovider', [ServiceRecordController::class, 'listByProvider'])->name('service_record.listByProvider'); // Listar atendimentos por provedor
-    Route::delete('/{id}', [ServiceRecordController::class, 'destroy'])->name('service_record.destroy'); // Cancelar um atendimento (atualizando o status)
+    Route::post('/', [ServiceRecordController::class, 'store'])->name('service_record.store');
+    Route::get('/listmy', [ServiceRecordController::class, 'listMy'])->name('service_record.listMy');
+    Route::get('/listbyclient', [ServiceRecordController::class, 'listByClient'])->name('service_record.listByClient');
+    Route::get('/listbyentity', [ServiceRecordController::class, 'listByEntity'])->name('service_record.listByEntity');
+    Route::get('/listbyprovider', [ServiceRecordController::class, 'listByProvider'])->name('service_record.listByProvider');
+    Route::delete('/{id}', [ServiceRecordController::class, 'destroy'])->name('service_record.destroy');
 });
