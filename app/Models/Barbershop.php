c5747d09ada4563d011ae3309321b88948126c8c
petertecnetdev <petertecnet@gmail.com>
2025-03-18 13:37:42 -0300

    model barbersho

--- app/Models/Barbershop.php
+++ app/Models/Barbershop.php
@@ -36,6 +36,9 @@ class Barbershop extends Model
         'terms_of_service',
         'social_media_links',
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 00b7790 (model barbersho)
         'barbers',
         'schema_markup',
         'language',
@@ -52,9 +55,12 @@ class Barbershop extends Model
         'twitter_title',
         'twitter_description',
         'twitter_image'
+<<<<<<< HEAD
 =======
         'barbers' 
 >>>>>>> cb0135b (solving conflit develop and staging on local)
+=======
+>>>>>>> 00b7790 (model barbersho)
     ];
 
     protected $casts = [
