90634983f67a69e0c1418a5bc310e57cef640390
petertecnetdev <petertecnet@gmail.com>
2025-06-26 12:36:48 +0000

    cors

--- config/cors.php
+++ config/cors.php
@@ -6,22 +6,13 @@ return [
 
     'allowed_methods' => ['*'],
 
-<<<<<<< HEAD
     // Se vocÃª nÃ£o precisa enviar cookies ou autenticaÃ§Ã£o via credenciais, defina como false.
     // Usar '*' com suporte a credenciais (true) nÃ£o Ã© permitido.
-=======
-    // Se você não precisa enviar cookies ou autenticação via credenciais, defina como false.
-    // Usar '*' com suporte a credenciais (true) não é permitido.
->>>>>>> 823736e (corns on vps)
     'supports_credentials' => false,
 
     'allowed_origins' => ['*'],
 
-<<<<<<< HEAD
     // Se 'allowed_origins' for '*', nÃ£o Ã© necessÃ¡rio definir padrÃµes de origem.
-=======
-    // Se 'allowed_origins' for '*', não é necessário definir padrões de origem.
->>>>>>> 823736e (corns on vps)
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
