d2f83243c20fe0117c69831ba62ac85473188257
petertecnetdev <petertecnet@gmail.com>
2025-07-14 11:03:50 -0300

    view of establishmentcontroller

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\{Appointment, Item, User};
+use App\Models\{Appointment, Item, Barbershop};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
