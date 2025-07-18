b31c3cb6543026834283d349da0b173e914be09f
petertecnetdev <petertecnet@gmail.com>
2025-07-18 13:59:50 -0300

    store appointment

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Carbon\Carbon;
+use Illuminate\Validation\Rule;   
 
 class AppointmentController extends Controller
 {
