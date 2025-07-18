83fbbf18ca6efe727bf0298100d81b6283c1b0fc
petertecnetdev <petertecnet@gmail.com>
2025-07-18 17:25:56 -0300

    store appointment

--- routes/api.php
+++ routes/api.php
@@ -245,21 +245,19 @@ Route::group([
 });
 
 
-
 Route::group([
     'middleware' => 'api',
-    'prefix' => 'establishment/{establishment}/menu'
 ], function () {
-    Route::get('/', [MenuController::class, 'listByEstablishment'])->name('menu.listByEstablishment');
-    Route::get('/{id}', [MenuController::class, 'show'])->name('menu.show');
-    Route::get('/view/{slug}', [MenuController::class, 'view'])->name('menu.view');
+    // Public routes
+    Route::get('menu',          [MenuController::class, 'list']) ->name('menu.list');
+    Route::get('menu/{id}',     [MenuController::class, 'show']) ->name('menu.show');
 });
 
 Route::group([
-    'middleware' => ['api', 'auth:api'],
-    'prefix' => 'establishment/{establishment}/menu'
+    'middleware' => ['api','auth:api'],
 ], function () {
-    Route::post('/', [MenuController::class, 'store'])->name('menu.store');
-    Route::post('/{id}', [MenuController::class, 'update'])->name('menu.update');
-    Route::delete('/{id}', [MenuController::class, 'destroy'])->name('menu.destroy');
+    // Protected routes
+    Route::post('menu',         [MenuController::class, 'store'])  ->name('menu.store');
+    Route::put('menu/{id}',     [MenuController::class, 'update']) ->name('menu.update');
+    Route::delete('menu/{id}',  [MenuController::class, 'destroy'])->name('menu.destroy');
 });
