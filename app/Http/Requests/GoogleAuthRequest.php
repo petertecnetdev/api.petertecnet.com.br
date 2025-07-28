8c4396fc0f62db4d2671dd23fec0cb6c71349c28
petertecnetdev <petertecnet@gmail.com>
2025-07-28 00:57:51 -0300

    Auth

---
+++ app/Http/Requests/GoogleAuthRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class GoogleAuthRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'token_id' => 'required|string',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'token_id.required' => 'O token do Google é obrigatório.',
+            'token_id.string'   => 'O token deve ser uma string.',
+        ];
+    }
+}
