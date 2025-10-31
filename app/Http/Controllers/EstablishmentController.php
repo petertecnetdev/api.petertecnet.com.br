ca6fb1690557d5f149d789730fda36669b19d996
petertecnetdev <petertecnet@gmail.com>
2025-10-31 03:13:45 -0300

    logo

--- app/Http/Controllers/EstablishmentController.php
+++ app/Http/Controllers/EstablishmentController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Validation\ValidationException;
 use Intervention\Image\Facades\Image;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 
 class EstablishmentController extends Controller
 {
