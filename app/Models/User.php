6d35c6cdc17fa1bf46d1d8a7ad345a22952b84ce
petertecnetdev <petertecnet@gmail.com>
2025-11-15 20:51:52 -0300

    all

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use App\Models\Traits\HasFiles;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use HasFactory, Notifiable;
+    use HasFactory, HasFiles, Notifiable;
 
     protected $fillable = [
         'user_name',
---
+++ app/Traits/HasFiles.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models\Traits;
+
+use App\Models\File;
+
+trait HasFiles
+{
+    public function files()
+    {
+        return $this->morphMany(
+            File::class,
+            'entity',
+            'entity_name',
+            'entity_id'
+        )
+        ->orderBy('is_primary', 'desc')
+        ->orderBy('sort_order')
+        ->orderBy('position')
+        ->orderBy('id');
+    }
+
+    public function primaryImage()
+    {
+        return $this->files()->where('is_primary', true)->first();
+    }
+
+    public function logoImage()
+    {
+        return $this->files()->where('type', 'logo')->first();
+    }
+
+    public function backgroundImage()
+    {
+        return $this->files()->where('type', 'background')->first();
+    }
+
+    public function galleryImages()
+    {
+        return $this->files()->where('type', 'gallery')->get();
+    }
+
+    public function avatarImage()
+    {
+        return $this->files()->where('type', 'avatar')->first();
+    }
+}
