4446ee1b49911a81e02bb9bf1f9c1942017f27ee
petertecnetdev <petertecnet@gmail.com>
2025-11-17 16:48:01 -0300

    all

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
 use Illuminate\Validation\ValidationException;
-use App\Mail\{ResendVerificationCodeMail, ResetPasswordMail};
+use App\Mail\{ResendVerificationCodeMail, InviteCompleteMail, ResetPasswordMail};
 use Validator;
 use Exception;
 use App\Http\Requests\GoogleAuthRequest;
