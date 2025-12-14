122cf970dbc5a0da5fcfbbd504a27bed8713b8ed
petertecnetdev <petertecnet@gmail.com>
2025-12-14 00:22:44 -0300

    all

--- routes/api.php
+++ routes/api.php
@@ -207,7 +207,6 @@ Route::prefix('establishment')->middleware(['api', 'auth:api'])->group(function 
     Route::get('/user', [EstablishmentController::class, 'listByUser'])->name('establishment.listByUser');
     Route::get('/my/category/{category}', [EstablishmentController::class, 'listMyByCategory'])->name('establishment.listMyByCategory');
 });
-
 /*
 |--------------------------------------------------------------------------
 | ORDERS
@@ -216,6 +215,11 @@ Route::prefix('establishment')->middleware(['api', 'auth:api'])->group(function 
 Route::prefix('order')->middleware(['api', 'auth:api'])->group(function () {
 
     Route::post('/', [OrderController::class, 'store'])->name('order.store');
+
+    Route::get('/list-by-entity/{slug}', [OrderController::class, 'listByEntitySlug'])
+        ->where('slug', '[A-Za-z0-9\-]+')
+        ->name('order.listByEntitySlug');
+
     Route::get('/listbyentity', [OrderController::class, 'listByEntity'])->name('order.listByEntity');
     Route::get('/listbyemployer', [OrderController::class, 'listByEmployer'])->name('order.listByEmployer');
 
