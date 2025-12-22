8ce4847caf14a710a0d45675b7a2b56a4b39de31
petertecnetdev <petertecnet@gmail.com>
2025-12-22 00:14:05 -0300

    all

--- app/Http/Controllers/EmployerController.php
+++ app/Http/Controllers/EmployerController.php
@@ -133,9 +133,10 @@ class EmployerController extends Controller
         // Se city ou uf não forem fornecidos, tenta obter pelo IP
         if (!$city || !$uf) {
             $ip = $request->ip();
-            $location = geoip($ip); // Assumindo que você tenha um pacote GeoIP configurado
-            $city = $city ?? $location->city;
-            $uf = $uf ?? $location->state;
+         $location = GeoIP::getLocation($request->ip());
+$city = $city ?? $location->city;
+$uf = $uf ?? $location->state;
+
         }
 
         $employers = Employer::whereHas('establishment', function ($q) use ($app_id, $city, $uf) {
