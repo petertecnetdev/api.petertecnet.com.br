0b72c13d99506a3aa695ae0afb37244e97dcb888
Peter Tecnet <petertecnet@gmail.com>
2026-08-28 08:11:25 -0300

    Configure Google OAuth client ID

--- config/services.php
+++ config/services.php
@@ -10,7 +10,7 @@ return [
     | This file is for storing the credentials for third party services such
     | as Mailgun, Postmark, AWS and more. This file provides the de facto
     | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
+    | a conventional location for credentials.
     |
     */
 
@@ -29,6 +29,10 @@ return [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+
+    'google' => [
+        'client_id' => env('GOOGLE_CLIENT_ID', env('GOOGLE_OAUTH_CLIENT_ID')),
     ],
 
 ];
