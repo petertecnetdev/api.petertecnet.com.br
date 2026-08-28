de3d49a01d72e28c3d14c070378655f2c909dd69
Peter Tecnet <petertecnet@gmail.com>
2026-08-28 15:24:12 -0300

    security(orders): register order context middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -46,5 +46,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'order.access' => \App\Http\Middleware\EnsureOrderAccess::class,
+        'order.context' => \App\Http\Middleware\EnsureOrderContext::class,
     ];
 }
