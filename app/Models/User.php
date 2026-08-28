d5700e903733e73a1ff0c671718cb3d3f4d427eb
Peter Tecnet <petertecnet@gmail.com>
2026-08-28 15:18:28 -0300

    feat(auth): cast and protect verification code expiry

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ class User extends Authenticatable implements JWTSubject
     use HasFactory, HasFiles, Notifiable;
 
     protected $fillable = [
-        'user_name', 'first_name', 'last_name', 'email', 'verification_code', 'password',
+        'user_name', 'first_name', 'last_name', 'email', 'verification_code', 'verification_code_expires_at', 'password',
         'reset_password_code', 'reset_password_expires_at', 'remember_token', 'profile_id',
         'cpf', 'google_id', 'avatar', 'address', 'phone', 'city', 'uf', 'postal_code', 'birthdate',
         'gender', 'marital_status', 'occupation', 'about', 'favorite_artist', 'favorite_genre',
@@ -26,12 +26,14 @@ class User extends Authenticatable implements JWTSubject
         'password',
         'remember_token',
         'verification_code',
+        'verification_code_expires_at',
         'reset_password_code',
         'reset_password_expires_at',
     ];
 
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'verification_code_expires_at' => 'datetime',
         'reset_password_expires_at' => 'datetime',
         'birthdate' => 'date',
         'extra_info' => 'array',
