1290bc046c13bc351e7df59c62cd9ddeaedc3cd0
Peter Tecnet <petertecnet@gmail.com>
2026-08-28 18:44:20 -0300

    fix: expose app-scoped authenticated session context

--- routes/account.php
+++ routes/account.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\AccountController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('account')->middleware(['api', 'auth:api'])->group(function () {
+    Route::get('/context', [AccountController::class, 'context'])->name('account.context');
     Route::post('/profile', [AccountController::class, 'updateProfile'])->name('account.profile.update');
     Route::post('/email/request-change', [AccountController::class, 'requestEmailChange'])->middleware('throttle:5,1')->name('account.email.requestChange');
     Route::post('/email/confirm-change', [AccountController::class, 'confirmEmailChange'])->middleware('throttle:10,1')->name('account.email.confirmChange');
