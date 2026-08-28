051ec5cb5ee9ddbbc1e722655011ccb045bb3f00
Peter Tecnet <petertecnet@gmail.com>
2026-08-28 20:23:16 -0300

    feat: expose barbershop agenda management route

--- routes/rasoio.php
+++ routes/rasoio.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('rasoio')->middleware(['api', 'auth:api'])->group(function () {
     Route::get('/orders/employer', [RasoioWorkflowController::class, 'employerOrders']);
+    Route::get('/establishments/{slug}/orders', [RasoioWorkflowController::class, 'establishmentOrders']);
     Route::patch('/orders/{id}/transition', [RasoioWorkflowController::class, 'transition'])->whereNumber('id');
     Route::patch('/orders/{id}/assign', [RasoioWorkflowController::class, 'assign'])->whereNumber('id');
     Route::get('/users/{userName}', [RasoioWorkflowController::class, 'userProfile']);
