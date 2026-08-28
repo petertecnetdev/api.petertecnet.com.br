f0631409000998b949308f13fa7f8d83c8b2b448
Peter Tecnet <petertecnet@gmail.com>
2026-08-28 19:47:36 -0300

    feat: add explicit v1 metrics routes

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Api\V1\EmployerController;
 use App\Http\Controllers\Api\V1\EstablishmentController;
 use App\Http\Controllers\Api\V1\ItemController;
+use App\Http\Controllers\Api\V1\MetricsController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1/apps/{application}')
@@ -18,11 +19,13 @@ Route::prefix('v1/apps/{application}')
             Route::post('/establishments', [EstablishmentController::class, 'store']);
             Route::patch('/establishments/{establishment}', [EstablishmentController::class, 'update']);
             Route::delete('/establishments/{establishment}', [EstablishmentController::class, 'destroy']);
+            Route::get('/establishments/{establishment}/metrics', [MetricsController::class, 'establishment']);
 
             Route::get('/establishments/{establishment}/items', [ItemController::class, 'mine']);
             Route::post('/items', [ItemController::class, 'store']);
             Route::patch('/items/{item}', [ItemController::class, 'update']);
             Route::delete('/items/{item}', [ItemController::class, 'destroy']);
+            Route::get('/items/{item}/metrics', [MetricsController::class, 'item']);
 
             Route::get('/establishments/{establishment}/employers', [EmployerController::class, 'index']);
             Route::post('/employers', [EmployerController::class, 'store']);
