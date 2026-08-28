7be5c47eba04e324b48299aca8e04741dba8ac59
Peter Tecnet <petertecnet@gmail.com>
2026-08-28 20:58:03 -0300

    feat: add Rasoio appointment detail endpoint

--- routes/rasoio.php
+++ routes/rasoio.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('rasoio')->middleware(['api', 'auth:api'])->group(function () {
     Route::get('/orders/employer', [RasoioWorkflowController::class, 'employerOrders']);
+    Route::get('/orders/{id}', [RasoioWorkflowController::class, 'orderDetail'])->whereNumber('id');
     Route::get('/establishments/{slug}/orders', [RasoioWorkflowController::class, 'establishmentOrders']);
     Route::patch('/orders/{id}/transition', [RasoioWorkflowController::class, 'transition'])->whereNumber('id');
     Route::patch('/orders/{id}/assign', [RasoioWorkflowController::class, 'assign'])->whereNumber('id');
