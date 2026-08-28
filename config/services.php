f0c0b91025846c75d5a7b7d880a7dba2cbb43969
Peter Tecnet <petertecnet@gmail.com>
2026-08-28 01:42:32 -0300

    security: centralize EFI service configuration

--- config/services.php
+++ config/services.php
@@ -7,10 +7,9 @@ return [
     | Third Party Services
     |--------------------------------------------------------------------------
     |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
+    | Centralize external service configuration here. Application code should
+    | consume config() values instead of calling env() directly so config cache
+    | works consistently in production.
     |
     */
 
@@ -29,6 +28,14 @@ return [
         'key' => env('AWS_ACCESS_KEY_ID'),
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
+    ],
+
+    'efi' => [
+        'base_url' => env('EFI_API_BASE_URL'),
+        'client_id' => env('EFI_CLIENT_ID'),
+        'client_secret' => env('EFI_CLIENT_SECRET'),
+        'cert_path' => env('EFI_CERT_PATH'),
+        'timeout' => (int) env('EFI_API_TIMEOUT', 15),
     ],
 
 ];
