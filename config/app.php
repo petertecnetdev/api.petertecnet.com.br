cbcac3863fac0525010384e22f64d5fd42f509a1
Peter Tecnet <petertecnet@gmail.com>
2026-08-29 20:53:21 -0300

    feat: enable application broadcast provider

--- config/app.php
+++ config/app.php
@@ -38,7 +38,7 @@ $providers = [
      */
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
-    // App\Providers\BroadcastServiceProvider::class,
+    App\Providers\BroadcastServiceProvider::class,
     App\Providers\EventServiceProvider::class,
     App\Providers\RouteServiceProvider::class,
 
