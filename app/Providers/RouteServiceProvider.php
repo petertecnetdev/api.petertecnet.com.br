7e7ef8f3f0426d3458fa7089b5218e98953d89bc
Peter Tecnet <petertecnet@gmail.com>
2026-08-29 13:50:04 -0300

    feat: load ecosystem governance routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -33,6 +33,10 @@ class RouteServiceProvider extends ServiceProvider
                 ->prefix('api')
                 ->group(base_path('routes/rasoio.php'));
 
+            Route::middleware('api')
+                ->prefix('api')
+                ->group(base_path('routes/ecosystem.php'));
+
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
