9faf07e8767fe30b7065080f80a95e2afddd1fe8
Peter Tecnet <petertecnet@gmail.com>
2026-08-28 21:36:35 -0300

    feat: add central admin routes

---
+++ routes/admin.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\Admin\SystemController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('admin')->middleware(['auth:api', \App\Http\Middleware\EnsureCentralAdmin::class])->group(function () {
+    Route::get('/dashboard', [SystemController::class, 'dashboard']);
+    Route::get('/users', [SystemController::class, 'users']);
+    Route::post('/users', [SystemController::class, 'storeUser']);
+    Route::put('/users/{user}', [SystemController::class, 'updateUser'])->whereNumber('user');
+    Route::delete('/users/{user}', [SystemController::class, 'destroyUser'])->whereNumber('user');
+    Route::patch('/users/{user}/status', [SystemController::class, 'setUserStatus'])->whereNumber('user');
+    Route::get('/profiles', [SystemController::class, 'profiles']);
+    Route::get('/users/{user}/applications', [SystemController::class, 'applicationsAccess'])->whereNumber('user');
+    Route::put('/users/{user}/applications/{application}', [SystemController::class, 'setApplicationAccess'])->whereNumber(['user','application']);
+    Route::get('/audit-logs', [SystemController::class, 'auditLogs']);
+});
