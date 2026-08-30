8c15d291385f8c24706636a8749b150b37efdf6f
Peter Tecnet <petertecnet@gmail.com>
2026-08-30 17:14:47 -0300

    feat(payflow): register PayFlow API routes

---
+++ routes/payflow.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\PayflowController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('payflow')->middleware(['api', 'auth:api'])->group(function () {
+    Route::get('/dashboard', [PayflowController::class, 'dashboard']);
+
+    Route::get('/contacts', [PayflowController::class, 'contacts']);
+    Route::post('/contacts', [PayflowController::class, 'storeContact']);
+
+    Route::get('/opportunities', [PayflowController::class, 'opportunities']);
+    Route::post('/opportunities', [PayflowController::class, 'storeOpportunity']);
+    Route::patch('/opportunities/{id}/stage', [PayflowController::class, 'updateOpportunityStage'])
+        ->whereNumber('id');
+});
