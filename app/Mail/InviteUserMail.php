8ac953716f37e5554db338dcc789fb8a33a8755b
Peter Tecnet <petertecnet@gmail.com>
2026-08-30 16:43:55 -0300

    feat: include application activation link in invite

--- app/Mail/InviteUserMail.php
+++ app/Mail/InviteUserMail.php
@@ -13,23 +13,32 @@ class InviteUserMail extends Mailable
     public $user;
     public $code;
     public $appName;
+    public $appUrl;
+    public $activationUrl;
 
-    public function __construct($user, $code, string $appName)
+    public function __construct($user, string $code, string $appName, string $appUrl)
     {
         $this->user = $user;
         $this->code = $code;
         $this->appName = trim($appName) ?: 'Plataforma Peter Tecnet';
+        $this->appUrl = rtrim($appUrl, '/');
+        $this->activationUrl = $this->appUrl.'/invite-complete?'.http_build_query([
+            'email' => $user->email,
+            'code' => $code,
+        ]);
     }
 
     public function build()
     {
         return $this
-            ->subject("Convite para acessar {$this->appName}")
+            ->subject("Sua nova conta no {$this->appName}")
             ->view('emails.invite-user')
             ->with([
                 'user' => $this->user,
                 'code' => $this->code,
                 'appName' => $this->appName,
+                'appUrl' => $this->appUrl,
+                'activationUrl' => $this->activationUrl,
             ]);
     }
 }
