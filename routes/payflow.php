2893e420d92925043c2eb09cfb5e2cc4a4a37696
Peter Tecnet <petertecnet@gmail.com>
2026-08-30 18:52:56 -0300

    feat(payflow): expose commercial MVP routes

--- routes/payflow.php
+++ routes/payflow.php
@@ -9,9 +9,17 @@ Route::prefix('payflow')->middleware(['api', 'auth:api'])->group(function () {
 
     Route::get('/contacts', [PayflowController::class, 'contacts']);
     Route::post('/contacts', [PayflowController::class, 'storeContact']);
+    Route::put('/contacts/{id}', [PayflowController::class, 'updateContact'])->whereNumber('id');
+    Route::delete('/contacts/{id}', [PayflowController::class, 'destroyContact'])->whereNumber('id');
 
     Route::get('/opportunities', [PayflowController::class, 'opportunities']);
     Route::post('/opportunities', [PayflowController::class, 'storeOpportunity']);
-    Route::patch('/opportunities/{id}/stage', [PayflowController::class, 'updateOpportunityStage'])
-        ->whereNumber('id');
+    Route::patch('/opportunities/{id}/stage', [PayflowController::class, 'updateOpportunityStage'])->whereNumber('id');
+
+    Route::get('/proposals', [PayflowController::class, 'proposals']);
+    Route::post('/proposals', [PayflowController::class, 'storeProposal']);
+
+    Route::get('/charges', [PayflowController::class, 'charges']);
+    Route::post('/charges', [PayflowController::class, 'storeCharge']);
+    Route::patch('/charges/{id}/paid', [PayflowController::class, 'markChargePaid'])->whereNumber('id');
 });
