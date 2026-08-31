73a4a26b0fa55fff2f54308874e0a760eae0eb39
Peter Tecnet <petertecnet@gmail.com>
2026-08-31 16:36:16 -0300

    feat(cutinapp): expose ticket issuance relation

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -2,4 +2,4 @@
 namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-class Ticket extends Model{use HasFactory;protected $fillable=['app_slug','event_id','name','type','price','limit_date','ticket_type','quantity','description'];protected $casts=['price'=>'decimal:2','quantity'=>'integer','limit_date'=>'datetime'];protected $table='tickets';protected $primaryKey='id';public function event(){return $this->belongsTo(Event::class);}public function production(){return $this->belongsTo(Production::class);}}
+class Ticket extends Model{use HasFactory;protected $fillable=['app_slug','event_id','name','type','price','limit_date','ticket_type','quantity','description'];protected $casts=['price'=>'decimal:2','quantity'=>'integer','limit_date'=>'datetime'];protected $table='tickets';protected $primaryKey='id';public function event(){return $this->belongsTo(Event::class);}public function production(){return $this->belongsTo(Production::class);}public function passes(){return $this->hasMany(EventPass::class,'ticket_id');}}
