046ab8befec076e17b2e7020b3557cf3bfbe7ea2
Peter Tecnet <petertecnet@gmail.com>
2026-09-01 12:02:58 -0300

    feat(cutinapp): configure commerce fees and EFI payout keys

--- config/services.php
+++ config/services.php
@@ -28,6 +28,13 @@ return [
         'client_secret' => env('EFI_CLIENT_SECRET'),
         'cert_path' => env('EFI_CERT_PATH'),
         'timeout' => (int) env('EFI_API_TIMEOUT', 15),
+        'pix_key' => env('EFI_PIX_KEY'),
+        'payout_source_pix_key' => env('EFI_PAYOUT_SOURCE_PIX_KEY', env('EFI_PIX_KEY')),
+    ],
+
+    'cutinapp' => [
+        'platform_fee_percent' => (float) env('CUTINAPP_PLATFORM_FEE_PERCENT', 8),
+        'webhook_token' => env('CUTINAPP_PAYMENT_WEBHOOK_TOKEN'),
     ],
 
     'geo_ip' => [
