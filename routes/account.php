36f90b6822529899f0c19dfcd53544ff20aec796
Peter Tecnet <petertecnet@gmail.com>
2026-09-01 10:13:48 -0300

    feat: expose Peter Account ecosystem and SSO routes

--- routes/account.php
+++ routes/account.php
@@ -1,10 +1,21 @@
 <?php
 
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\EcosystemAccountController;
+use App\Http\Controllers\EcosystemSsoController;
+use App\Http\Controllers\SafeAccountContextController;
 use Illuminate\Support\Facades\Route;
 
+Route::post('account/sso/exchange', [EcosystemSsoController::class, 'exchange'])
+    ->middleware(['api', 'throttle:20,1'])
+    ->name('account.sso.exchange');
+
 Route::prefix('account')->middleware(['api', 'auth:api'])->group(function () {
-    Route::get('/context', [AccountController::class, 'context'])->name('account.context');
+    Route::get('/ecosystem', [EcosystemAccountController::class, 'show'])->name('account.ecosystem');
+    Route::post('/sso/handoff', [EcosystemSsoController::class, 'createHandoff'])
+        ->middleware('throttle:30,1')
+        ->name('account.sso.handoff');
+    Route::get('/context', [SafeAccountContextController::class, 'show'])->name('account.context');
     Route::get('/item-metrics', [AccountController::class, 'itemMetrics'])->name('account.itemMetrics');
     Route::post('/profile', [AccountController::class, 'updateProfile'])->name('account.profile.update');
     Route::post('/email/request-change', [AccountController::class, 'requestEmailChange'])->middleware('throttle:5,1')->name('account.email.requestChange');
