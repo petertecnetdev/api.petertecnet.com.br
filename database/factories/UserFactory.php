58bff283107bc4963875b3b137b0315a49f10fd8
Peter Tecnet <petertecnet@gmail.com>
2026-09-02 00:36:56 -0300

    fix(tests): use supported faker user formats

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,10 +18,10 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'first_name' => fake()->first_name(),
-            'last_name' => fake()->last_name(),
-            'phone' => fake()->phone(),
-            'email' => fake()->safeEmail(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'phone' => fake()->phoneNumber(),
+            'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
