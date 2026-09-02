46b75f0719f24f23fac9e1566ad63c9659f0db24
Peter Tecnet <petertecnet@gmail.com>
2026-09-02 00:41:01 -0300

    feat(platform): expose developer portal and openapi spec

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -11,6 +9,11 @@ Route::get('/', function () {
 Route::get('/api', function () {
     return response()->json(['message' => 'API está pronta para ser usada']);
 });
+
+Route::get('/developers', fn () => view('developers'))->name('developers');
+Route::get('/developers/openapi.yaml', fn () => response()->file(base_path('docs/openapi.yaml'), [
+    'Content-Type' => 'application/yaml; charset=utf-8',
+]));
 
 Route::get('/login', function () {
     return response()->json(['message' => 'Usuário não autenticado']);
