d486ca092c987eed304d985938cf9f1588f67c24
Peter Tecnet <petertecnet@gmail.com>
2026-09-02 00:23:13 -0300

    test(cutinapp): isolate producer contract gate from unrelated suites

--- app/Http/Controllers/CutinappContractProtectedEventController.php
+++ app/Http/Controllers/CutinappContractProtectedEventController.php
@@ -11,7 +11,7 @@ class CutinappContractProtectedEventController extends CutinappEventController
     public function store(Request $request)
     {
         $productionId = (int) $request->input('production_id');
-        if ($productionId > 0) {
+        if (config('cutinapp.enforce_producer_contract', true) && $productionId > 0) {
             $signed = DB::table('cutinapp_producer_contract_acceptances')
                 ->where('production_id', $productionId)
                 ->where('contract_version', CutinappProducerContractService::VERSION)
