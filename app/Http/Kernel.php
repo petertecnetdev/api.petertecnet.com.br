797a51cadc2112621666b40151bec6aded06c9ea
Peter Tecnet <petertecnet@gmail.com>
2026-09-03 16:46:57 -0300

    feat(identity): register step-up middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -55,5 +55,6 @@ class Kernel extends HttpKernel
         'compatibility.route' => \App\Http\Middleware\MarkCompatibilityRoute::class,
         'producer.agreement' => \App\Http\Middleware\EnsureProducerAgreement::class,
         'token.version' => \App\Http\Middleware\EnsureTokenVersion::class,
+        'identity.step-up' => \App\Http\Middleware\RequireIdentityStepUp::class,
     ];
 }
