6b0cb8688ce38bd5018d96b657e8e2564f94a287
Peter Tecnet <petertecnet@gmail.com>
2026-09-03 18:26:11 -0300

    feat(developers): register public API security middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,5 +56,10 @@ class Kernel extends HttpKernel
         'compatibility.route' => \App\Http\Middleware\MarkCompatibilityRoute::class,
         'producer.agreement' => \App\Http\Middleware\EnsureProducerAgreement::class,
         'token.version' => \App\Http\Middleware\EnsureTokenVersion::class,
+        'developer.key' => \App\Http\Middleware\AuthenticateDeveloperKey::class,
+        'developer.scope' => \App\Http\Middleware\RequireDeveloperScope::class,
+        'developer.origin' => \App\Http\Middleware\ValidateDeveloperOrigin::class,
+        'developer.track' => \App\Http\Middleware\TrackDeveloperRequest::class,
+        'api.version' => \App\Http\Middleware\ApiVersionHeaders::class,
     ];
 }
