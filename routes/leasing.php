56ca0810c24f2f7ec47579bbae6b0d3b24d20b6a
Peter Tecnet <petertecnet@gmail.com>
2026-09-03 17:43:35 -0300

    feat(leasing): expose application-scoped leasing contract

---
+++ routes/leasing.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Domain\Leasing\Http\Controllers\LeasingController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1/apps/{application}')
+    ->middleware(['app.context', 'auth:api', 'token.version', 'app.capability:leasing'])
+    ->group(function () {
+        Route::get('/leasing/dashboard', [LeasingController::class, 'dashboard']);
+
+        Route::get('/properties', [LeasingController::class, 'properties']);
+        Route::post('/properties', [LeasingController::class, 'storeProperty']);
+        Route::match(['put', 'patch'], '/properties/{propertyId}', [LeasingController::class, 'updateProperty'])->whereNumber('propertyId');
+        Route::delete('/properties/{propertyId}', [LeasingController::class, 'destroyProperty'])->whereNumber('propertyId');
+        Route::get('/properties/{propertyId}/inspections', [LeasingController::class, 'inspections'])->whereNumber('propertyId');
+        Route::post('/properties/{propertyId}/inspections', [LeasingController::class, 'storeInspection'])->whereNumber('propertyId');
+
+        Route::get('/leases', [LeasingController::class, 'leases']);
+        Route::post('/leases', [LeasingController::class, 'storeLease']);
+        Route::get('/leases/{leaseId}', [LeasingController::class, 'showLease'])->whereNumber('leaseId');
+        Route::match(['put', 'patch'], '/leases/{leaseId}', [LeasingController::class, 'updateLease'])->whereNumber('leaseId');
+        Route::post('/leases/{leaseId}/contract/generate', [LeasingController::class, 'generateContract'])->whereNumber('leaseId');
+        Route::post('/leases/{leaseId}/contract/send', [LeasingController::class, 'sendContract'])->whereNumber('leaseId')->middleware('throttle:10,1');
+        Route::post('/leases/{leaseId}/contract/sign', [LeasingController::class, 'sign'])->whereNumber('leaseId')->middleware('throttle:20,1');
+
+        Route::get('/leases/{leaseId}/documents', [LeasingController::class, 'documents'])->whereNumber('leaseId');
+        Route::post('/leases/{leaseId}/documents', [LeasingController::class, 'uploadDocument'])->whereNumber('leaseId')->middleware('throttle:30,1');
+        Route::get('/leases/{leaseId}/documents/{documentId}', [LeasingController::class, 'downloadDocument'])->whereNumber('leaseId')->whereNumber('documentId');
+        Route::delete('/leases/{leaseId}/documents/{documentId}', [LeasingController::class, 'deleteDocument'])->whereNumber('leaseId')->whereNumber('documentId');
+
+        Route::get('/leases/{leaseId}/charges', [LeasingController::class, 'charges'])->whereNumber('leaseId');
+        Route::post('/leases/{leaseId}/charges', [LeasingController::class, 'storeCharge'])->whereNumber('leaseId');
+        Route::post('/leases/{leaseId}/charges/schedule', [LeasingController::class, 'generateRentSchedule'])->whereNumber('leaseId');
+        Route::post('/leases/{leaseId}/charges/{chargeId}/payment', [LeasingController::class, 'preparePayment'])->whereNumber('leaseId')->whereNumber('chargeId')->middleware('throttle:20,1');
+        Route::patch('/leases/{leaseId}/charges/{chargeId}/paid', [LeasingController::class, 'markChargePaid'])->whereNumber('leaseId')->whereNumber('chargeId');
+    });
