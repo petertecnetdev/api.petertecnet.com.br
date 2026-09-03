e7cb1deeaa35ddbae43881425d95277f80927b17
Peter Tecnet <petertecnet@gmail.com>
2026-09-03 10:13:16 -0300

    feat(finance): configure pending payment expiration

--- config/commerce.php
+++ config/commerce.php
@@ -5,6 +5,7 @@ use App\Services\Payments\Gateways\MercadoPagoPaymentGateway;
 return [
     'payments' => [
         'default' => env('COMMERCE_PAYMENT_PROVIDER', 'mercadopago'),
+        'pending_expiration_minutes' => (int) env('COMMERCE_PENDING_EXPIRATION_MINUTES', 30),
 
         'providers' => [
             'mercadopago' => MercadoPagoPaymentGateway::class,
