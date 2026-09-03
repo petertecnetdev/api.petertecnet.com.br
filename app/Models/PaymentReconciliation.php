389d3121ee54a3adaebed5684c7cfbfb370cb372
Peter Tecnet <petertecnet@gmail.com>
2026-09-03 10:09:46 -0300

    feat(finance): add reconciliation model

---
+++ app/Models/PaymentReconciliation.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PaymentReconciliation extends Model
+{
+    protected $fillable = [
+        'payment_id', 'provider', 'provider_payment_id', 'local_status', 'remote_status',
+        'local_amount', 'remote_amount', 'local_provider_fee', 'remote_provider_fee',
+        'matched', 'discrepancy_code', 'details', 'checked_at',
+    ];
+
+    protected $casts = [
+        'local_amount' => 'decimal:2',
+        'remote_amount' => 'decimal:2',
+        'local_provider_fee' => 'decimal:2',
+        'remote_provider_fee' => 'decimal:2',
+        'matched' => 'boolean',
+        'details' => 'array',
+        'checked_at' => 'datetime',
+    ];
+}
