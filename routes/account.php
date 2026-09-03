37fd6ee91a312c0004b305caaec568951d5cdf57
Peter Tecnet <petertecnet@gmail.com>
2026-09-03 12:48:10 -0300

    feat(auth): expose global ecosystem SSO session routes

--- routes/account.php
+++ routes/account.php
@@ -10,11 +10,22 @@ Route::post('account/sso/exchange', [EcosystemSsoController::class, 'exchange'])
     ->middleware(['api', 'throttle:20,1'])
     ->name('account.sso.exchange');
 
+Route::post('account/sso/session/exchange', [EcosystemSsoController::class, 'exchangeGlobalSession'])
+    ->middleware(['api', 'throttle:30,1'])
+    ->name('account.sso.session.exchange');
+
+Route::delete('account/sso/session', [EcosystemSsoController::class, 'revokeGlobalSession'])
+    ->middleware(['api', 'throttle:30,1'])
+    ->name('account.sso.session.revoke');
+
 Route::prefix('account')->middleware(['api', 'auth:api'])->group(function () {
     Route::get('/ecosystem', [EcosystemAccountController::class, 'show'])->name('account.ecosystem');
     Route::post('/sso/handoff', [EcosystemSsoController::class, 'createHandoff'])
         ->middleware('throttle:30,1')
         ->name('account.sso.handoff');
+    Route::post('/sso/session', [EcosystemSsoController::class, 'establishGlobalSession'])
+        ->middleware('throttle:30,1')
+        ->name('account.sso.session.establish');
     Route::get('/context', [SafeAccountContextController::class, 'show'])->name('account.context');
     Route::get('/item-metrics', [AccountController::class, 'itemMetrics'])->name('account.itemMetrics');
     Route::post('/profile', [AccountController::class, 'updateProfile'])->name('account.profile.update');
