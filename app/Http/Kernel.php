d6d5250442730d5f83e134c7b24dbab1f34ca8ed
Peter Tecnet <petertecnet@gmail.com>
2026-09-03 18:27:12 -0300

    feat(developers): isolate dynamic browser CORS

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -10,6 +10,7 @@ class Kernel extends HttpKernel
         // \App\Http\Middleware\TrustHosts::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\RequestId::class,
+        \App\Http\Middleware\HandleDeveloperCors::class,
         \Illuminate\Http\Middleware\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
