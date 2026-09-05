bd6c8d463020b191bc76d3cce01e0aa8c27432c3
Peter Tecnet <petertecnet@gmail.com>
2026-09-05 14:45:38 -0300

    feat: relacionar respostas, anexos e reações

--- app/Models/AppMessage.php
+++ app/Models/AppMessage.php
@@ -4,13 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class AppMessage extends Model
 {
     protected $fillable = [
         'conversation_id',
         'sender_user_id',
+        'reply_to_message_id',
         'type',
+        'client_token',
         'body',
         'metadata',
         'edited_at',
@@ -31,5 +34,20 @@ class AppMessage extends Model
     public function sender(): BelongsTo
     {
         return $this->belongsTo(User::class, 'sender_user_id');
+    }
+
+    public function replyTo(): BelongsTo
+    {
+        return $this->belongsTo(self::class, 'reply_to_message_id');
+    }
+
+    public function attachments(): HasMany
+    {
+        return $this->hasMany(AppMessageAttachment::class, 'message_id');
+    }
+
+    public function reactions(): HasMany
+    {
+        return $this->hasMany(AppMessageReaction::class, 'message_id');
     }
 }
