c86ed9e587ef28a2a19b7531f7583b506bca096b
Peter Tecnet <petertecnet@gmail.com>
2026-09-05 00:26:20 -0300

    feat: add subscription payment model

---
+++ app/Models/SubscriptionPayment.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SubscriptionPayment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'subscription_id',
+        'provider_payment_id',
+        'provider_invoice_id',
+        'status',
+        'amount',
+        'currency',
+        'paid_at',
+        'raw_payload',
+    ];
+
+    protected $casts = [
+        'amount' => 'decimal:2',
+        'paid_at' => 'datetime',
+        'raw_payload' => 'array',
+    ];
+
+    public function subscription()
+    {
+        return $this->belongsTo(Subscription::class);
+    }
+}
