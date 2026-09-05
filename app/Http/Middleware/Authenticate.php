3dcad538b15275812c1f3f9a908cd8788922d760
Peter Tecnet <petertecnet@gmail.com>
2026-09-05 00:45:39 -0300

    feat: enforce subscription after API authentication

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Middleware;
 
+use App\Services\SubscriptionAccessGuard;
+use Closure;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
+    public function handle($request, Closure $next, ...$guards)
+    {
+        return parent::handle($request, function ($authenticatedRequest) use ($next) {
+            app(SubscriptionAccessGuard::class)->enforce($authenticatedRequest);
+
+            return $next($authenticatedRequest);
+        }, ...$guards);
+    }
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
