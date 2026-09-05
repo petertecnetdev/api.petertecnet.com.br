45ca0974f4eaba7bb18046ed1dac60d894522e5e
Peter Tecnet <petertecnet@gmail.com>
2026-09-05 14:25:58 -0300

    feat(social): register participant community routes

---
+++ routes/participants.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Domain\Social\Http\Controllers\ParticipantSocialController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1/apps/{application}')
+    ->middleware(['app.context', 'auth:api', 'token.version', 'app.capability:social'])
+    ->group(function () {
+        Route::get('/participants', [ParticipantSocialController::class, 'index'])->middleware('throttle:120,1');
+        Route::get('/participants/activity', [ParticipantSocialController::class, 'activity'])->middleware('throttle:120,1');
+        Route::get('/participants/{participantId}', [ParticipantSocialController::class, 'show'])->whereNumber('participantId')->middleware('throttle:120,1');
+        Route::post('/participants/{participantId}/follow', [ParticipantSocialController::class, 'follow'])->whereNumber('participantId')->middleware('throttle:60,1');
+        Route::delete('/participants/{participantId}/follow', [ParticipantSocialController::class, 'unfollow'])->whereNumber('participantId')->middleware('throttle:60,1');
+    });
