6f84cea8afc7fab89b97bad8415d439addf66a6a
Peter Tecnet <petertecnet@gmail.com>
2026-09-05 00:27:42 -0300

    feat: register subscription routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -37,6 +37,7 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('api')->prefix('api')->group(base_path('routes/ecosystem.php'));
             Route::middleware('api')->prefix('api')->group(base_path('routes/cognition.php'));
             Route::middleware('api')->prefix('api')->group(base_path('routes/market_data.php'));
+            Route::middleware('api')->prefix('api')->group(base_path('routes/subscriptions.php'));
             Route::middleware('web')->group(base_path('routes/web.php'));
         });
     }
