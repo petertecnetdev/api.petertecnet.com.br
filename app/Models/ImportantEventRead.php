ad4fa08f0d8e331e6ea955e6ecf2e0b63b9d1811
Peter Tecnet <petertecnet@gmail.com>
2026-09-06 19:01:37 -0300

    feat(events): add important event read state

---
+++ app/Models/ImportantEventRead.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ImportantEventRead extends Model
+{
+    protected $fillable = ['important_event_id', 'user_id', 'read_at'];
+
+    protected $casts = [
+        'important_event_id' => 'integer',
+        'user_id' => 'integer',
+        'read_at' => 'datetime',
+    ];
+
+    public function event()
+    {
+        return $this->belongsTo(ImportantEvent::class, 'important_event_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
