3df50f4c7672d52bf39ba3f38081ba9312657bf0
Peter Tecnet <petertecnet@gmail.com>
2026-09-06 06:38:42 -0300

    fix(telemetry): expect cache-safe runtime version

--- config/telemetry.php
+++ config/telemetry.php
@@ -2,7 +2,7 @@
 
 return [
     'frontend_schema' => env('PETER_TELEMETRY_SCHEMA', '3'),
-    'frontend_version' => env('PETER_TELEMETRY_VERSION', '3.3.1'),
+    'frontend_version' => env('PETER_TELEMETRY_VERSION', '3.3.2'),
     'stale_minutes' => (int) env('PETER_TELEMETRY_STALE_MINUTES', 60),
     'down_minutes' => (int) env('PETER_TELEMETRY_DOWN_MINUTES', 1440),
 ];
