2e2c4487d0fbef0cb6bc6679d6f743cbd5df7a9b
Peter Tecnet <petertecnet@gmail.com>
2026-09-07 12:03:05 -0300

    feat(creative): add text generation route

--- routes/creative.php
+++ routes/creative.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Domain\Creative\Http\Controllers\CreativeGenerationController;
+use App\Domain\Creative\Http\Controllers\CreativeTextGenerationController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1/apps/{application}')
@@ -8,4 +9,6 @@ Route::prefix('v1/apps/{application}')
     ->group(function () {
         Route::post('/creative/images', [CreativeGenerationController::class, 'image'])
             ->middleware('throttle:6,1');
+        Route::post('/creative/texts', [CreativeTextGenerationController::class, 'text'])
+            ->middleware('throttle:20,1');
     });
