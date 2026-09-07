894b9cd7f5edde82c37487bf0817e0343df2f7bb
Peter Tecnet <petertecnet@gmail.com>
2026-09-07 04:32:11 -0300

    feat(admin): route bulk event deletion

--- routes/admin_establishment_resources.php
+++ routes/admin_establishment_resources.php
@@ -12,6 +12,8 @@ Route::prefix('admin/ecosystem/establishments/{establishment}/resources')
     ->group(function () {
         Route::get('/context', [AdminEstablishmentResourceController::class, 'context']);
         Route::get('/events', [AdminEstablishmentEventController::class, 'index']);
+        Route::delete('/events', [AdminEstablishmentEventController::class, 'destroyMany'])
+            ->middleware('throttle:20,1');
         Route::post('/events/{event}/duplicate', [AdminEstablishmentEventController::class, 'duplicate'])
             ->whereNumber('event')
             ->middleware('throttle:30,1');
