37466df51bc61173f8ce9070703e1529a3a2a511
Peter Tecnet <petertecnet@gmail.com>
2026-09-07 00:34:29 -0300

    feat(creative): register generic image generation endpoint

---
+++ routes/creative.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Domain\Creative\Http\Controllers\CreativeGenerationController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('v1/apps/{application}')
+    ->middleware(['app.context', 'auth:api', 'token.version', 'app.capability:events'])
+    ->group(function () {
+        Route::post('/creative/images', [CreativeGenerationController::class, 'image'])
+            ->middleware('throttle:6,1');
+    });
