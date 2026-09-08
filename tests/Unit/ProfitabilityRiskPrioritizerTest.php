2bdb9f5e73de2c5c2bc8a16f8cf5e122b3488ebe
Peter Tecnet <petertecnet@gmail.com>
2026-09-08 11:04:52 -0300

    test(analytics): cover confidence-adjusted recovery value

--- tests/Unit/ProfitabilityRiskPrioritizerTest.php
+++ tests/Unit/ProfitabilityRiskPrioritizerTest.php
@@ -141,6 +141,9 @@ final class ProfitabilityRiskPrioritizerTest extends TestCase
         self::assertSame(20.0, $result[0]['checkout_recovery_break_even_conversion_rate']);
         self::assertSame(30.0, $result[0]['checkout_recovery_conversion_safety_margin']);
         self::assertSame(5.38, $result[0]['checkout_recovery_confidence_margin_to_break_even']);
+        self::assertSame(15.23, $result[0]['recovery_confidence_adjusted_platform_contribution']);
+        self::assertSame(3.23, $result[0]['recovery_confidence_adjusted_net_platform_contribution']);
+        self::assertSame(26.92, $result[0]['recovery_confidence_adjusted_roi_percent']);
         self::assertSame(2.5, $result[0]['recovery_contribution_per_attempt']);
         self::assertSame(5.0, $result[0]['recovery_contribution_per_recovered_order']);
         self::assertSame(10, $result[0]['recovery_decision_minimum_attempts']);
@@ -226,6 +229,53 @@ final class ProfitabilityRiskPrioritizerTest extends TestCase
 
         self::assertCount(1, $result);
         self::assertSame('collect_more_data', $result[0]['recovery_decision']);
+    }
+
+    public function test_it_prioritizes_scaling_by_confidence_adjusted_net_contribution(): void
+    {
+        $result = (new ProfitabilityRiskPrioritizer())->prioritize([
+            [
+                'payment_method' => 'pix',
+                'platform_revenue' => 100.0,
+                'platform_contribution_after_processing' => 100.0,
+                'platform_contribution_shortfall' => 0.0,
+                'platform_loss_making_orders' => 0,
+                'platform_loss_making_gross_revenue' => 0.0,
+                'platform_collection_effective_fee_rate' => 3.0,
+                'platform_collection_break_even_fee_rate' => 1.0,
+                'platform_collection_fee_rate_gap_to_break_even' => 0.0,
+                'platform_collection_sustainable' => true,
+                'checkout_recovery_attempts' => 30,
+                'checkout_recovered_orders' => 18,
+                'recovered_platform_revenue' => 90.0,
+                'gross_at_risk' => 300.0,
+            ],
+            [
+                'payment_method' => 'credit_card',
+                'platform_revenue' => 100.0,
+                'platform_contribution_after_processing' => 100.0,
+                'platform_contribution_shortfall' => 0.0,
+                'platform_loss_making_orders' => 0,
+                'platform_loss_making_gross_revenue' => 0.0,
+                'platform_collection_effective_fee_rate' => 3.0,
+                'platform_collection_break_even_fee_rate' => 1.0,
+                'platform_collection_fee_rate_gap_to_break_even' => 0.0,
+                'platform_collection_sustainable' => true,
+                'checkout_recovery_attempts' => 12,
+                'checkout_recovered_orders' => 6,
+                'recovered_platform_revenue' => 60.0,
+                'gross_at_risk' => 300.0,
+            ],
+        ], ['pix' => 1.0, 'credit_card' => 1.0]);
+
+        self::assertCount(2, $result);
+        self::assertSame('pix', $result[0]['payment_method']);
+        self::assertSame('scale_carefully', $result[0]['recovery_decision']);
+        self::assertSame('credit_card', $result[1]['payment_method']);
+        self::assertGreaterThan(
+            $result[1]['recovery_confidence_adjusted_net_platform_contribution'],
+            $result[0]['recovery_confidence_adjusted_net_platform_contribution']
+        );
     }
 
     public function test_it_does_not_invent_recovery_roi_without_configured_cost(): void
