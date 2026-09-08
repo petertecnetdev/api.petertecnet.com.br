5b20728afcac049377759a03453fe79b3a7cc4ff
Peter Tecnet <petertecnet@gmail.com>
2026-09-08 15:53:41 -0300

    feat: expose producer engagement email admin endpoints

--- routes/application_admin.php
+++ routes/application_admin.php
@@ -30,6 +30,12 @@ Route::prefix('v1/apps/{application}')
             Route::put('/productions/{production}', [ApplicationAdminProductionController::class, 'update'])
                 ->whereNumber('production')
                 ->middleware([EnsureApplicationAdmin::class.':establishments.manage', 'throttle:30,1']);
+            Route::get('/productions/{production}/engagement-email/preview', [ApplicationAdminProductionController::class, 'engagementPreview'])
+                ->whereNumber('production')
+                ->middleware([EnsureApplicationAdmin::class.':establishments.view', 'throttle:30,1']);
+            Route::post('/productions/{production}/engagement-email', [ApplicationAdminProductionController::class, 'sendEngagementEmail'])
+                ->whereNumber('production')
+                ->middleware([EnsureApplicationAdmin::class.':establishments.manage', 'throttle:10,1']);
 
             Route::get('/events', [ApplicationAdminEventController::class, 'index'])
                 ->middleware(EnsureApplicationAdmin::class.':events.view');
