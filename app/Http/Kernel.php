f8b8d842be20bdcb3ea137e65a322d76741d2d3f
Peter Tecnet <petertecnet@gmail.com>
2026-09-09 11:47:41 -0300

    feat: enforce impersonation on all API requests

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -30,6 +30,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\HandleImpersonation::class,
             \App\Http\Middleware\NormalizeHomeLocationFilters::class,
             \App\Http\Middleware\EnsureOrderContext::class,
             \App\Http\Middleware\TrackApiInteraction::class,
