4f67b0587d21b0d5b5fbbc7c901dbe6d38e850fa
Peter Tecnet <petertecnet@gmail.com>
2026-09-11 02:53:27 -0300

    feat: expose evolved messaging routes

--- routes/content.php
+++ routes/content.php
@@ -36,9 +36,14 @@ Route::prefix('v1')->group(function () {
             Route::post('/messaging/direct', [MessagingController::class, 'openDirect'])->middleware('throttle:60,1');
             Route::get('/messaging/conversations/{conversationId}', [MessagingController::class, 'showConversation'])->whereNumber('conversationId')->middleware('throttle:180,1');
             Route::get('/messaging/conversations/{conversationId}/messages', [MessagingController::class, 'messages'])->whereNumber('conversationId')->middleware('throttle:240,1');
-            Route::post('/messaging/conversations/{conversationId}/messages', [MessagingController::class, 'send'])->whereNumber('conversationId')->middleware('throttle:120,1');
-            Route::post('/messaging/conversations/{conversationId}/read', [MessagingController::class, 'markRead'])->whereNumber('conversationId')->middleware('throttle:180,1');
+            Route::post('/messaging/conversations/{conversationId}/messages', [MessagingController::class, 'send'])->whereNumber('conversationId')->middleware('throttle:90,1');
+            Route::post('/messaging/conversations/{conversationId}/delivered', [MessagingController::class, 'markDelivered'])->whereNumber('conversationId')->middleware('throttle:240,1');
+            Route::post('/messaging/conversations/{conversationId}/read', [MessagingController::class, 'markRead'])->whereNumber('conversationId')->middleware('throttle:240,1');
+            Route::patch('/messaging/conversations/{conversationId}/state', [MessagingController::class, 'state'])->whereNumber('conversationId')->middleware('throttle:120,1');
             Route::delete('/messaging/conversations/{conversationId}', [MessagingController::class, 'archive'])->whereNumber('conversationId')->middleware('throttle:60,1');
+            Route::patch('/messaging/messages/{messageId}', [MessagingController::class, 'updateMessage'])->whereNumber('messageId')->middleware('throttle:90,1');
+            Route::delete('/messaging/messages/{messageId}', [MessagingController::class, 'deleteMessage'])->whereNumber('messageId')->middleware('throttle:60,1');
+            Route::post('/messaging/messages/{messageId}/reaction', [MessagingController::class, 'reaction'])->whereNumber('messageId')->middleware('throttle:180,1');
         });
 
     Route::prefix('discovery')->group(function () {
@@ -70,7 +75,7 @@ Route::middleware(['auth:api', 'token.version', \App\Http\Middleware\PeterTecnet
     Route::get('/content', [ContentManagementController::class, 'index']);
     Route::post('/content', [ContentManagementController::class, 'store']);
     Route::patch('/content/{content}', [ContentManagementController::class, 'update'])->whereNumber('content');
-    Route::post('/content/{content}/publish', [ContentManagementController::class, 'publish'])->whereNumber('content');
+    Route::post('/content/{content}/publish', [ContentManagementController::class, 'publish']);
     Route::delete('/content/{content}', [ContentManagementController::class, 'destroy'])->whereNumber('content');
     Route::get('/discovery/analytics', [DiscoveryAnalyticsController::class, 'summary']);
     Route::get('/discovery/web-vitals', [WebVitalAnalyticsController::class, 'index']);
