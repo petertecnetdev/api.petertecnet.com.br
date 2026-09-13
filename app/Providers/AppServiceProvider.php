ead7ec7a0c42e41698ac539de1f707968045c12d
Peter Tecnet <petertecnet@gmail.com>
2026-09-13 17:27:12 -0300

    fix(order): register context integrity guard

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,7 @@ use App\Observers\EstablishmentOwnershipNotificationObserver;
 use App\Observers\EventFeedActivityObserver;
 use App\Observers\EventProducerNotificationObserver;
 use App\Observers\InteractionAuditObserver;
+use App\Observers\OrderContextIntegrityObserver;
 use App\Observers\OrderPricingObserver;
 use App\Observers\OrderScheduleGuardObserver;
 use App\Services\AsaasPayoutService;
@@ -74,6 +75,7 @@ class AppServiceProvider extends ServiceProvider
             $auditedModel::observe(InteractionAuditObserver::class);
         }
 
+        Order::observe(OrderContextIntegrityObserver::class);
         Order::observe(OrderPricingObserver::class);
         Order::observe(OrderScheduleGuardObserver::class);
         Establishment::observe(EstablishmentOwnershipNotificationObserver::class);
