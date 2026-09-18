de6aa2ce0d6bc712c9f07d2a8ec57823fd02cef4
Peter Tecnet <petertecnet@gmail.com>
2026-09-18 16:38:12 -0300

    feat(onboarding): guide producer from contract to payout setup

--- resources/views/emails/producer-agreement-welcome.blade.php
+++ resources/views/emails/producer-agreement-welcome.blade.php
@@ -5,5 +5,7 @@
 <p>O termo da organização <strong>{{ $organization->name }}</strong> foi assinado para uso da aplicação <strong>{{ $application->name ?? 'Peter Tecnet' }}</strong>.</p>
 <p>Versão: {{ $acceptance->contract_version }}<br>Aceite: {{ $acceptance->accepted_at }}</p>
 <p>A cópia em PDF está anexada a esta mensagem.</p>
+<p>O próximo passo é confirmar os dados de recebimento e cadastrar uma chave Pix válida. As vendas são liberadas somente quando contrato e recebimentos estiverem regulares.</p>
+<p><a href="{{ rtrim((string) ($application->url ?? 'https://petertecnet.com.br'), '/') }}/producer/finance?production={{ $organization->id }}&focus=activation">Configurar recebimentos</a></p>
 <p>Peter Tecnet</p>
 </body></html>
