b1d76cd26afa18af6b819dcebfd35195728864c7
Peter Tecnet <petertecnet@gmail.com>
2026-09-26 18:54:34 -0300

    chore(seed): include Cutinapp growth content

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ProfileSeeder::class,
             AdminUserSeeder::class,
+            CutinappGrowthContentSeeder::class,
         ]);
     }
 }
